05dfc64d7b88d250373ea172ed5c736948843b1e
senthil-chandramohan <164313674+senthil-chandramohan@users.noreply.github.com>
2024-07-22 10:22:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
-import React from 'react';
+// import React from 'react';
+// import HomePage from './Components/homepage';
+
+// class App extends React.Component {
+//   value = {
+//     key1: 'Home ',
+//     key2: 'Page'
+//   };
+
+//   state = {
+//     receivedFromHomePage: 'Waiting for button click'
+//   }
+
+//   handleData = (data) => {
+//     this.setState({ receivedFromHomePage: data });
+//   };
+
+//   render() {  
+//     return (
+//       <div className="App">
+//         <h1>Welcome to React</h1>
+//         <p>Received data from child: {this.state.receivedFromHomePage}</p>
+//         <div>
+//           <HomePage handler={this.handleData} pageContent={this.value}></HomePage>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
 import HomePage from './Components/homepage';
 
-class App extends React.Component {
-  value = {
+const App = () => {
+  const value = {
     key1: 'Home ',
     key2: 'Page'
   };
 
-  state = {
-    receivedFromHomePage: 'Waiting for button click'
-  }
+  const [receivedFromHomePage, setReceivedFromHomePage] = useState('Waiting for button click');
 
-  handleData = (data) => {
-    this.setState({ receivedFromHomePage: data });
+  const handleData = (data) => {
+    setReceivedFromHomePage(data);
   };
 
-  render() {  
-    return (
-      <div className="App">
-        <h1>Welcome to React</h1>
-        <p>Received data from child: {this.state.receivedFromHomePage}</p>
-        <div>
-          <HomePage handler={this.handleData} pageContent={this.value}></HomePage>
-        </div>
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <p>Received data from child: {receivedFromHomePage}</p>
+      <div>
+        <HomePage handler={handleData} pageContent={value}></HomePage>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
