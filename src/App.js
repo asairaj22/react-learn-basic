8c4f491322b773aa803b4524b94a2fd308e7d66c
Chandramohan <2047100@cognizant.com>
2025-02-12 16:29:16 +0530

    file updated

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-// import { LoadingProvider } from './components/common/LoadingContext';
-// import Loader from './components/common/loader';
-// import { ToastProvider } from './components/common/ToastContext';
-import { LoadingProvider } from 'components/common/LoadingContext';
-import Loader from 'components/common/loader';
-import { ToastProvider } from 'components/common/ToastContext';
+import { LoadingProvider } from './components/common/LoadingContext';
+import Loader from './components/common/loader';
+import { ToastProvider } from './components/common/ToastContext';
 
 const App = () => {
   const navigate = useNavigate();
