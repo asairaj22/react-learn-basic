6d27945ea3ea167f6a0852c07cfeb58be294175c
asairaj22 <asairaj22@gmail.com>
2025-03-21 11:31:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter basename="/react-basic">
+     <BrowserRouter basename="/react-learn-basic">
       <App />
     </BrowserRouter>
   </React.StrictMode>
